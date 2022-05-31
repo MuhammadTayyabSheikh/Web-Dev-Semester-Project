cc5306de87b40a69e200312b037664f61020dc08
Muhammad Tayyab <98114526+MuhammadTayyabSheikh@users.noreply.github.com>
2022-05-31 18:11:36 +0500

    creayed server for serving notes

---
+++ backend/data/notes.js
@@ -0,0 +1,23 @@
+const notes = [{
+    "id": 1,
+    "title": "❤️ 💔 💌 💕 💞 💓 💗 💖 💘 💝 💟 💜 💛 💚 💙",
+    "content": "Universal fault-tolerant definition"
+}, {
+    "id": 2,
+    "title": "ÅÍÎÏ˝ÓÔÒÚÆ☃",
+    "content": "Switchable high-level alliance"
+}, {
+    "id": 3,
+    "title": "‪‪test‪",
+    "content": "Realigned web-enabled core"
+}, {
+    "id": 4,
+    "title": "NIL",
+    "content": "Progressive responsive success"
+}, {
+    "id": 5,
+    "title": "הָיְתָהtestالصفحات التّحول",
+    "content": "Object-based logistical productivity"
+}];
+
+module.exports = notes;
