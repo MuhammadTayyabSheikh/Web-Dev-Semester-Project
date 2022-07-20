513b40b1023942067192ce1ffeffb2998c8b1e10
Muhammad Tayyab <98114526+MuhammadTayyabSheikh@users.noreply.github.com>
2022-07-20 17:50:48 +0500

    .

---
+++ src/actions/userActions.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { USER_LOGIN_FAILURE, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT_REQUEST, USER_REGISTER_FAILURE, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_FAILURE, USER_UPDATE_REQUEST } from "../constants/userConstants";
+import { USER_UPDATE_SUCCESS } from './../constants/userConstants';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "https://two-note-backend.herokuapp.com/api/users/login/",
+      {
+        email,
+        password,
+      },
+      config
+    );
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAILURE,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+}
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT_REQUEST });
+}
+
+export const register = (name, email, password, img) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "https://two-note-backend.herokuapp.com/api/users/",
+      {
+        name,
+        email,
+        password,
+        img,
+      },
+      config
+    );
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAILURE,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+}
+
+export const updateUser = (name, email, password, img) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
+    console.log(name, email, password, img);
+    const { userLogin: { userInfo } } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post('https://two-note-backend.herokuapp.com/api/users/profile/', {
+      name, email, password, img
+    }, config);
+
+    console.log(data, "action");
+    dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAILURE,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+}
