95f5bdafa7f7c8689477a7223d7479331dbfb007
Muhammad Tayyab <98114526+MuhammadTayyabSheikh@users.noreply.github.com>
2022-09-26 23:34:36 +0500

    .

---
+++ build/precache-manifest.6b22c82ad2a98ea3f9fb4c98ee2545ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a449a909cca52fb762c",
+    "url": "/static/css/main.bb162cf3.chunk.css"
+  },
+  {
+    "revision": "9a449a909cca52fb762c",
+    "url": "/static/js/main.c631bf68.chunk.js"
+  },
+  {
+    "revision": "8d1b24a60328ef7e83b3",
+    "url": "/static/js/runtime~main.fbfcb512.js"
+  },
+  {
+    "revision": "f06d800681592d9128fe",
+    "url": "/static/js/2.270a2055.chunk.js"
+  },
+  {
+    "revision": "d0c7ccbb85f77c99a826",
+    "url": "/static/js/3.91095169.chunk.js"
+  },
+  {
+    "revision": "664c2c99381f76f927c5d5895286b1a0",
+    "url": "/static/media/background6.664c2c99.jpg"
+  },
+  {
+    "revision": "123f952bad5de66ff7364a7897a3600d",
+    "url": "/index.html"
+  }
+];
